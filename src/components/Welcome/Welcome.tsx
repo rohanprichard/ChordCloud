ef0657d1f7423d657768f947af6b1f5e5401d5bf
rohanprichard <rohanprichard@gmail.com>
2024-07-31 01:55:19 +0530

    update landing page

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -5,19 +5,40 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
+        Welcome to <br />
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+          ChordCloud
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Vite project includes a minimal setup, if you want to learn more on Mantine +
-        Vite integration follow{' '}
+        Let's get you started storing your files here. <br />
+        (Source code for this project is {' '}
         <Anchor href="https://mantine.dev/guides/vite/" size="lg">
-          this guide
+            here
         </Anchor>
-        . To get started edit pages/Home.page.tsx file.
+        ) 
       </Text>
+        <Text c="lime" ta="center" size="lg" maw={580} mx="auto" >
+        Contact me via email to get access
+        </Text>
     </>
   );
+  // return (
+  //   <>
+  //     <Title className={classes.title} ta="center" mt={100}>
+  //       Welcome to{' '}
+  //       <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
+  //         Mantine
+  //       </Text>
+  //     </Title>
+  //     <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+  //       This starter Vite project includes a minimal setup, if you want to learn more on Mantine +
+  //       Vite integration follow{' '}
+  //       <Anchor href="https://mantine.dev/guides/vite/" size="lg">
+  //         this guide
+  //       </Anchor>
+  //       . To get started edit pages/Home.page.tsx file.
+  //     </Text>
+  //   </>
+  // );
 }
